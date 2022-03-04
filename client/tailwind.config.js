1539f84f6f5c35c3f30fef80349fd3949c6650ea
MOLO-7 <am8736419@gmail.com>
2022-03-04 14:39:29 -0800

    update theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    theme: {
-      extend: {},
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      nav: "#1a1a1a",
+      body: "#202225",
+      dark: "#072227",
+      blue: "#4c6ef5",
+      "blue-light": "#4FBDBA",
+      cyan: "#AEFEFF",
+      white: {
+        100: "#FFF",
+        200: "#FFFFFFb7",
+        300: "#FFFFFF66",
+        400: "#FFFFFF4d"
+      },
     },
-    variants: {
-      extend: {},
+    fontFamily: {
+      sans: ["poppins", "sans-serif"],
+      serif: ["Segoe UI", "Tahoma", "Geneva", "Verdana", "sans-serif"]
     },
-    plugins: [],
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
