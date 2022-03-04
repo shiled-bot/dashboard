cbd235055692f4b392be75df7a3c18646323b637
MOLO-7 <am8736419@gmail.com>
2022-03-04 14:37:43 -0800

    add login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/" element={<h1>Welcom To Home Page</h1>}></Route>
         <Route path="*" element={<h1>No Match Route</h1>}></Route>
       </Routes>
