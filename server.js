0b56e07494128749e8116cfb6b54e654c0103979
MOLO-7 <am8736419@gmail.com>
2022-03-04 14:32:31 -0800

    create routing & add cors

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require("express");
+import "dotenv/config";
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import parser from "body-parser"
+
+const bodyParser = parser.json();
 const app = express();
 const port = process.env.PORT || 8080;
+const { DB_URI, CLIENT_ORIGIN } = process.env;
 
-app.use((req, res) => {
-    res.json({
-        message: "Welcome",
-        code: 200
-    });
+app.use(bodyParser)
+app.use(cors({origin: CLIENT_ORIGIN}));
+
+// routers
+import loginRouter from "./routes/login.js";
+import isUserAuthRouter from "./routes/isUserAuth.js";
+
+app.use("/login", loginRouter);
+app.use("/isUserAuth", isUserAuthRouter)
+
+mongoose.connect(DB_URI)
+.then(() => {
+    app.listen(port, () => console.log('Server Is Running On Port ' + port));
 })
-
-app.listen(port, () => console.log('Listing on port ' + port));
+.catch(console.error);
