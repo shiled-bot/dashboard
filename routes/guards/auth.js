26c3eaf00d5541db34d1605bcbdc214755fb2e4b
MOLO-7 <am8736419@gmail.com>
2022-03-06 00:11:56 +0200

    update auth guard response messages to be capitalized

--- routes/guards/auth.js
+++ routes/guards/auth.js
@@ -3,10 +3,10 @@ import jwt from "jsonwebtoken"
 export default (req, res, next) => {
     const token = req.headers["x-access-token"]?.split(" ")[1];
     
-    if (!token) return res.status(401).json({ error: "unauthorized", isLoggedIn: false });
+    if (!token) return res.status(401).json({ error: "Unauthorized", isLoggedIn: false });
     
     jwt.verify(token, process.env.JWT_SECRET, (err, paylod) => {
-        if (err) return res.status(401).json({ error: "faild to authenticate", isLoggedIn: false });
+        if (err) return res.status(401).json({ error: "Faild To Authenticate", isLoggedIn: false });
 
         req.userId = paylod.id;
 
