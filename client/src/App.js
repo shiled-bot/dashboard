42521af031c91aae8d5be6cd054d3b63c48ebcff
MOLO-7 <am8736419@gmail.com>
2022-04-02 00:01:53 +0200

    create logout & 404 routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -8,8 +10,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
         <Route path="/" element={<h1>Welcom To Home Page</h1>}></Route>
-        <Route path="*" element={<h1>No Match Route</h1>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
