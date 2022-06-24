80bb013aadcbef2b28a4f1d42ef7ad4a36ecf23c
MOLO-7 <am8736419@gmail.com>
2022-06-24 14:41:55 +0200

    adjust font size for medium screens

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -5,7 +5,7 @@ const Error = ({
 }) => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen overflow-hidden flex flex-col items-center md:items-start justify-center px-4 md:pl-10">
-      <h2 className="text-white-100 text-4xl md:text-8xl font-bold text-center md:text-left">
+      <h2 className="text-white-100 text-4xl md:text-6xl font-bold text-center md:text-left">
         {title}
       </h2>
       <p className="text-white-200 mt-2 text-center text-sm md:text-base md:text-left">
